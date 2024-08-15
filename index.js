272bd8f37ec8409eabeea4165deb18807cf83ebc
Your Name <humphrey@paxdivitiae.com>
2024-08-15 09:41:55 +0200

    message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ AWS.config.update({ region: 'us-east-1' });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'UssdSessions';
 const provinces = [
-            "Limpopo", "Kzn", "Eastern Cape",
+            "humphrey", "Kzn", "Eastern Cape",
             "Northern Cape", "Western Cape", "Gauteng",
             "Free state", "North west", "Mpumalanga"
         ];
